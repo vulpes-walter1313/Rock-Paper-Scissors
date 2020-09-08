a570ad04578c05e9f689e962fa1af33231b1d7a0
Walter Fritsche <wfritsche95@protonmail.com>
2020-09-08 15:03:31 -0500

    fixed indentations of functions

--- main_gui.js
+++ main_gui.js
@@ -5,88 +5,88 @@ let playerWins = 0;
 // Declares players choice as a variable
 let playerChoice;
 
-  function computerPlay() {
-    // Randomly chose between Rock, Paper, or Scissors
-    const compChoice = Math.floor(Math.random() * 3) ;
-    let result;
+function computerPlay() {
+  // Randomly chose between Rock, Paper, or Scissors
+  const compChoice = Math.floor(Math.random() * 3) ;
+  let result;
 
-    switch (compChoice) {
-      case 0:
-        result = 'rock';
-        break;
+  switch (compChoice) {
+    case 0:
+      result = 'rock';
+      break;
 
-      case 1:
-        result = 'paper';
-        break;
-      case 2:
-        result = 'scissors';
-    }
-
-    return result;
+    case 1:
+      result = 'paper';
+      break;
+    case 2:
+      result = 'scissors';
   }
 
-  function playRound(playerSelection, computerSelection) {
-    // compares the player's joice vs computer's choice and returns
-    // a string whether you win or lose the round.
-    switch (playerSelection) {
-      case 'rock':
+  return result;
+}
+
+function playRound(playerSelection, computerSelection) {
+  // compares the player's joice vs computer's choice and returns
+  // a string whether you win or lose the round.
+  switch (playerSelection) {
+    case 'rock':
+      if (computerSelection === 'paper') {
+        compWins++;
+        return "You lose! Paper beats Rock!";
+      } else if (computerSelection === 'scissors') {
+        playerWins++;
+        return 'You win! Rock beats Scissors!';
+      } else {
+        // Implied that Computer's choice is rock
+        return "T'was a Tie! You both selected Rock!";
+      }
+      break;
+
+      case 'paper':
         if (computerSelection === 'paper') {
-          compWins++;
-          return "You lose! Paper beats Rock!";
+          return "T'was a Tie! You both selected Paper!";
         } else if (computerSelection === 'scissors') {
-          playerWins++;
-          return 'You win! Rock beats Scissors!';
+          compWins++;
+          return 'You lose! Scissors beats Paper!';
         } else {
-          // Implied that Computer's choice is rock
-          return "T'was a Tie! You both selected Rock!";
+          // implied that computers choice is rock
+          playerWins++;
+          return "You Win! Paper beats Rock!";
         }
         break;
 
-        case 'paper':
+        case 'scissors':
           if (computerSelection === 'paper') {
-            return "T'was a Tie! You both selected Paper!";
+            playerWins++;
+            return "You Win! Scissors beats Paper!";
           } else if (computerSelection === 'scissors') {
-            compWins++;
-            return 'You lose! Scissors beats Paper!';
+            return "T'was a Tie! You both selected Scissors!";
           } else {
             // implied that computers choice is rock
-            playerWins++;
-            return "You Win! Paper beats Rock!";
+            compWins++;
+            return "You Lose! Rock beats Scissors";
           }
           break;
+        default:
+        // this is in the event the user inputs anything other than the choices
+          return "You didn't choose an appropriate choice.\nReload and try again";
+  }
+}
 
-          case 'scissors':
-            if (computerSelection === 'paper') {
-              playerWins++;
-              return "You Win! Scissors beats Paper!";
-            } else if (computerSelection === 'scissors') {
-              return "T'was a Tie! You both selected Scissors!";
-            } else {
-              // implied that computers choice is rock
-              compWins++;
-              return "You Lose! Rock beats Scissors";
-            }
-            break;
-          default:
-          // this is in the event the user inputs anything other than the choices
-            return "You didn't choose an appropriate choice.\nReload and try again";
+function game(){
+
+  const btns = document.querySelectorAll('button');
+  btns.forEach(button => {
+    // console.log(button.value);
+    button.addEventListener('click', () => {
+      playerChoice = button.value;
+      let computerChoice = computerPlay();
+      // console.log(`Comp's choice: ${computerChoice}`);
+      // console.log(playRound(playerChoice, computerChoice));
+      const resultDisplay = document.querySelector('.results');
+      resultDisplay.textContent = playRound(playerChoice, computerChoice);
+      resultDisplay.textContent += `\n\nComp's choice: ${computerChoice}`;
     }
-  }
-
-  function game(){
-
-    const btns = document.querySelectorAll('button');
-    btns.forEach(button => {
-      // console.log(button.value);
-      button.addEventListener('click', () => {
-        playerChoice = button.value;
-        let computerChoice = computerPlay();
-        // console.log(`Comp's choice: ${computerChoice}`);
-        // console.log(playRound(playerChoice, computerChoice));
-        const resultDisplay = document.querySelector('.results');
-        resultDisplay.textContent = playRound(playerChoice, computerChoice);
-        resultDisplay.textContent += `\n\nComp's choice: ${computerChoice}`;
-      }
-    )});
-  }
-  game();
+  )});
+}
+game();
